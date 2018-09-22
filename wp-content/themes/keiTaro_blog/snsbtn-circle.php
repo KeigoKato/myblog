1ef51c7eccbd7961eec2d683c8ed03bf365eedc4
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-09-22 16:46:07 +0900

    snsボタンをレンプレート化

---
+++ wp-content/themes/keiTaro_blog/snsbtn-circle.php
@@ -0,0 +1,6 @@
+<div class="sns-list pt-2 pb-5 text-center">
+    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="twitter" class="border rounded-circle mx-2 fab fa-twitter sns-icon text-white"></a>
+    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="facebook" class="border rounded-circle mx-2 fab fa-facebook-f sns-icon text-white"></a>
+    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google+" class="border rounded-circle mx-2 fab fa-google-plus-g sns-icon text-white"></a>
+    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="RSS" class="border rounded-circle mx-2 fas fa-rss sns-icon text-white"></a>
+</div>
