760a09d458a62ec1a7fac6bcf907b4b3585204eb
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-09-23 20:11:14 +0900

    フォントを変更

--- wp-content/themes/keiTaro_blog/functions.php
+++ wp-content/themes/keiTaro_blog/functions.php
@@ -3,10 +3,10 @@
 require_once locate_template('lib/init.php');        // 初期設定の関数
 // require_once locate_template('lib/cleanup.php');     // 不要なモノを削除する関数
 // require_once locate_template('lib/titles.php');      // タイトル出力の関数
-// require_once locate_template('lib/breadcrumbs.php'); // パンくずリストの関数
 // require_once locate_template('lib/scripts.php');     // CSSやJavascript関連の関数
 // require_once locate_template('lib/ads.php');         // 広告関連の関数
 require_once locate_template('lib/widgets.php');     // サイドバー、ウィジェットの関数
-require_once locate_template('lib/breadclumb.php');     // パンくずリストを表示する関数
+require_once locate_template('lib/breadclumbs.php');     // パンくずリストを表示する関数
 require_once locate_template('lib/recommend.php');     // 人気記事一覧を取得する関数
+require_once locate_template('lib/pagination.php');     // ページネーションを実装する関数
 require_once locate_template('lib/custom.php');      // その他カスタマイズの関数
